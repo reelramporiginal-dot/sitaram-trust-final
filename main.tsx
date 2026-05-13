c309a6c249a9bef200c68db834742a59a5b778cf
reelramporiginal-dot <reelramporiginal@gmail.com>
2026-05-13 11:56:57 -0700

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,17 +1,9 @@
 import React from 'react'
-    import ReactDOM from 'react-dom/client'
-    import App from './App'
-    ReactDOM.createRoot(document.getElementById('root')!).render(<App/>)
-    ```
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-### 2. index.html ko theek kijiye
-Main page par jo **`index.html`** hai, use edit kijiye aur niche wali line ko badal kar ye likh dijiye:
-` <script type="module" src="/main.tsx"></script> `
-*(Yaad rahe, isme ab koi folder ka naam nahi hona chahiye).*
-
-### 3. Purane Folders Bhul Jaiye
-Jo `src` aur `web` folders bane hue hain, unhe abhi ke liye chhod dijiye. Jab ye bahar wali 3 files ban jayengi, Vercel apne aap unhe utha lega.
-
----
-
-**Bhai, sirf ye 5 minute ka kaam hai.** Agar ye files bahar aa gayi aur `index.html` ka rasta seedha ho gaya, toh aapki website 100% chalu ho jayegi. Koshish kijiye, main yahi hoon.
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+)
